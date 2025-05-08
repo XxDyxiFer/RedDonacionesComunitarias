81f1970eb0ba8cf136fb676202795bf15c7eaf08
XxDyxiFer <134980401+XxDyxiFer@users.noreply.github.com>
2025-05-08 18:41:11 -0500

    Errores en proceso

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,5 @@
 // routes.js
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import RegistroUsuario from './pages/RegistroUsuario';
 import PerfilUsuario from './pages/PerfilUsuario';
@@ -7,15 +7,13 @@ import ListaDonaciones from './pages/ListaDonaciones';
 import DetalleDonacion from './pages/DetalleDonacion';
 
 const AppRoutes = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/registro" element={<RegistroUsuario />} />
-      <Route path="/perfil" element={<PerfilUsuario />} />
-      <Route path="/donaciones" element={<ListaDonaciones />} />
-      <Route path="/donacion/:id" element={<DetalleDonacion />} />
-    </Routes>
-  </Router>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/registro" element={<RegistroUsuario />} />
+    <Route path="/perfil" element={<PerfilUsuario />} />
+    <Route path="/donaciones" element={<ListaDonaciones />} />
+    <Route path="/donacion/:id" element={<DetalleDonacion />} />
+  </Routes>
 );
 
 export default AppRoutes;
